4f5f7ba4fe041cde057ffac2148f14ab440ffc36
memonwasim <wasim@tasolglobal.com>
2014-09-26 17:14:24 +0530

    [code style] Removed spaces from defined function.

--- component/admin/ijoomeradv.php
+++ component/admin/ijoomeradv.php
@@ -11,23 +11,23 @@
 
 defined('_JEXEC') or die;
 
-defined ( 'DS' ) or define( 'DS', DIRECTORY_SEPARATOR );
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
 define('IJADV_VERSION',1.5);
 
-jimport ( 'joomla.version' );
-$version = new JVersion ( );
+jimport('joomla.version');
+$version = new JVersion();
 
 //define joomla version
-defined ( 'IJ_JOOMLA_VERSION' ) or define ( 'IJ_JOOMLA_VERSION', floatval ( $version->RELEASE ) );
+defined('IJ_JOOMLA_VERSION') or define('IJ_JOOMLA_VERSION', floatval ($version->RELEASE));
 
-defined ( 'IJ_ADMIN' )			or define ( 'IJ_ADMIN', JPATH_COMPONENT );
-defined ( 'IJ_SITE' )			or define ( 'IJ_SITE', JPATH_ROOT . DS . 'components' . DS . 'com_ijoomeradv' );
-defined ( 'IJ_ASSET' )			or define ( 'IJ_ASSET', IJ_ADMIN . DS . 'assets' );
-defined ( 'IJ_CONTROLLER' ) 	or define ( 'IJ_CONTROLLER', IJ_ADMIN . DS . 'controllers' );
-defined ( 'IJ_HELPER' )			or define ( 'IJ_HELPER', IJ_ADMIN . DS . 'helpers' );
-defined ( 'IJ_MODEL' )			or define ( 'IJ_MODEL', IJ_ADMIN . DS . 'models' );
-defined ( 'IJ_TABLE' )			or define ( 'IJ_TABLE', IJ_ADMIN . DS . 'tables' );
-defined ( 'IJ_VIEW' )			or define ( 'IJ_VIEW', IJ_ADMIN . DS . 'views' );
+defined('IJ_ADMIN')			or define('IJ_ADMIN', JPATH_COMPONENT);
+defined('IJ_SITE')			or define('IJ_SITE', JPATH_ROOT . DS . 'components' . DS . 'com_ijoomeradv');
+defined('IJ_ASSET')			or define('IJ_ASSET', IJ_ADMIN . DS . 'assets');
+defined('IJ_CONTROLLER') 	or define('IJ_CONTROLLER', IJ_ADMIN . DS . 'controllers' );
+defined('IJ_HELPER')		or define('IJ_HELPER', IJ_ADMIN . DS . 'helpers');
+defined('IJ_MODEL')			or define('IJ_MODEL', IJ_ADMIN . DS . 'models');
+defined('IJ_TABLE')			or define('IJ_TABLE', IJ_ADMIN . DS . 'tables');
+defined('IJ_VIEW')			or define('IJ_VIEW', IJ_ADMIN . DS . 'views');
 
 require_once IJ_HELPER.DS.'helper.php';
 
