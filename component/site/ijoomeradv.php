74788c291f022e68276d696fa12cb699cbbcce5d
iJoomer <dave.technosis@gmail.com>
2014-10-09 18:46:57 +0530

    Add document header

--- component/site/ijoomeradv.php
+++ component/site/ijoomeradv.php
@@ -1,7 +1,23 @@
 <?php
 /**
+ * iJoomer is a mobile platform which provides native applications for 
+ * iPhone, Android and BlackBerry and works in real-time sync with Joomla! 
+ * You can launch your very own Joomla! Mobile Apps on the respective appStore. 
+ * Users of your website will be able to download the Joomla Mobile Application 
+ * and install on the device.
+ * For more info visit: http://www.ijoomer.com
+ * For Technical Support: Forum - http://www.ijoomer.com/Forum/
+ * 
+ * iJoomer is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License 2 as published by the 
+ * Free Software Foundation.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with iJoomer; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 
  * @package     IJoomer.Frontend
- * @subpackage  com_ijoomeradv.views
+ * @subpackage  com_ijoomeradv
  *
  * @copyright   Copyright (C) 2010 - 2014 Tailored Solutions PVT. Ltd. All rights reserved.
  * @license     GNU General Public License version 2 or later; see LICENSE
