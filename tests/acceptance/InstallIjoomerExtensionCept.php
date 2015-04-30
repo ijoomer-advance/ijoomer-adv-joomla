090d27046aa7dbdf4c0f433e45887ee0d926774c
Dalpat <dalpatsingh@tasolglobal.com>
2015-04-30 12:48:59 +0530

    adding installing ijoomer components

---
+++ tests/acceptance/InstallIjoomerExtensionCept.php
@@ -0,0 +1,17 @@
+<?php
+$I = new AcceptanceTester($scenario);
+$I->wantTo('log in as regular user');
+$I->amOnPage('/administrator/index.php');
+$I->fillField('username','admin');
+$I->fillField('passwd','admin');
+$I->click('Log in');
+$I->waitForText('Control Panel',5);
+$I->click('//*[@id=\'menu\']/li[6]/a/span');
+$I->click('//*[@id=\'menu\']/li[6]/ul/li[1]/a');
+$I->waitForText('Extension Manager: Install',5);
+$I->click('Install from Directory');
+$I->fillField('//*[@id=\'install_directory\']','/var/www/html/ijoomer-adv-joomla');
+$I->Click('//input[contains(@onclick,\'Joomla.submitbutton3()\')]');
+$I->waitForText('Installing component was successful.',60);
+$I->wait(15);
+?>
