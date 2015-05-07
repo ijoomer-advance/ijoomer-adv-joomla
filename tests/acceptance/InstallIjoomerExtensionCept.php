2e6cc5d1031245eb955efb64e02f37258496e93b
Dalpat <dalpatsingh@tasolglobal.com>
2015-05-07 11:55:23 +0530

    update SigninCept.php

--- tests/acceptance/InstallIjoomerExtensionCept.php
+++ tests/acceptance/InstallIjoomerExtensionCept.php
@@ -10,8 +10,8 @@ $I->click('.//*[@id=\'menu\']/li[6]/a');
 $I->click('//*[@id=\'menu\']/li[6]/ul/li[1]/a');
 $I->waitForText('Extension Manager: Install',5);
 $I->click('Install from Directory');
-$I->fillField('//*[@id=\'install_directory\']','/home/travis/build/dsmewara/ijoomer-adv-joomla/');
-$I->Click('//input[contains(@onclick,\'Joomla.submitbutton3()\')]');
+$I->fillField('//*[@id=\'install_directory\']','/home/travis/build/ijoomer-advance/ijoomer-adv-joomla/');
+$I->Click('Install');
 $I->waitForText('Installing component was successful.',60);
 $I->wait(15);
 ?>
