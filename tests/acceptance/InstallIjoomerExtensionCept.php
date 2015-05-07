287c1a5ac5e5843584a8f40a40732505b6945142
Dalpat <dalpatsingh@tasolglobal.com>
2015-05-07 16:25:00 +0530

    update InstallIjoomerExtensionCept.php

--- tests/acceptance/InstallIjoomerExtensionCept.php
+++ tests/acceptance/InstallIjoomerExtensionCept.php
@@ -1,6 +1,7 @@
 <?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('log in as regular user');
+$path = '/var/www/html/ijoomer-adv-joomla/';
 $I->amOnPage('/administrator/index.php');
 $I->fillField('username','admin');
 $I->fillField('passwd','admin');
@@ -10,7 +11,7 @@ $I->click('.//*[@id=\'menu\']/li[6]/a');
 $I->click('//*[@id=\'menu\']/li[6]/ul/li[1]/a');
 $I->waitForText('Extension Manager: Install',5);
 $I->click('Install from Directory');
-$I->fillField('//*[@id=\'install_directory\']','/home/travis/build/ijoomer-advance/ijoomer-adv-joomla/');
+$I->fillField('//*[@id=\'install_directory\']',$path);
 $I->Click('//input[contains(@onclick,\'Joomla.submitbutton3()\')]');
 $I->wait(60);
 $I->see('Users');
