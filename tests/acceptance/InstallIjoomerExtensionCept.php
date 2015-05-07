4e9c6cc09d70ebf4b3d7f552778ad00b1938bd91
Dalpat <dalpatsingh@tasolglobal.com>
2015-05-07 12:53:17 +0530

    update travis.yml

--- tests/acceptance/InstallIjoomerExtensionCept.php
+++ tests/acceptance/InstallIjoomerExtensionCept.php
@@ -10,7 +10,7 @@ $I->click('.//*[@id=\'menu\']/li[6]/a');
 $I->click('//*[@id=\'menu\']/li[6]/ul/li[1]/a');
 $I->waitForText('Extension Manager: Install',5);
 $I->click('Install from Directory');
-$I->fillField('//*[@id=\'install_directory\']','/var/www/html/ijoomer-adv-joomla/');
+$I->fillField('//*[@id=\'install_directory\']','/home/travis/build/ijoomer-advance/ijoomer-adv-joomla/');
 $I->Click('//input[contains(@onclick,\'Joomla.submitbutton3()\')]');
 $I->wait(50);
 $I->see('Installing component was successful.');
