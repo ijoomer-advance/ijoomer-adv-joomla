92607d0092ef51fbc259f213505bbcab35f3b4ee
Dalpat <dalpatsingh@tasolglobal.com>
2015-05-20 11:19:44 +0530

    issue- #78 Adding ManagePushNotificationAdministratorCept.php

---
+++ tests/_pages/Joomla3/Administrator/GlobalConfigurationJ3Page.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * @package     ijoomer
+ * @subpackage  Page Class
+ * @copyright   Copyright (C) 2008 - 2015 ijoomer.com. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+/**
+ * Class CategoryManagerJ3Page
+ *
+ * @since  1.4
+ *
+ * @link   http://codeception.com/docs/07-AdvancedUsage#PageObjects
+ */
+class GlobalConfigurationJ3Page
+{
+	// Include url of current page
+	public static $URL = '/administrator/index.php?option=com_ijoomeradv&view=config';
+	
+	public static $GlobalConfigLink = "//a[text()='Global Config']";
+
+	public static $ThemeConfigLink = "//a[text()='Theme Config']";
+
+	public static $PushNotificationConfigLink = "//a[text()='Push Notification Config']";
+
+	public static $EncryptionLink = "//a[text()='Encryption']";
+	
+
+}
